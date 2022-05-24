3d2cfb90131c1366d500868316d32d84a7de64bd
CamBrown101 <cameron-brown@live.com>
2022-05-23 19:43:54 -0700

    end of l 2

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <table class="mail-table">
     <tbody>
       <tr
-        v-for="email in emails"
+        v-for="email in unarchivedEmails"
         :key="email.id"
         :class="['clickable', email.read ? 'read' : '']"
         @click="email.read = true"
@@ -15,17 +15,20 @@
             <strong>{{ email.subject }}</strong> - {{ email.body }}
           </p>
         </td>
-        <td class="date">{{ email.sentAt }}</td>
+        <td class="date">{{ format(new Date(email.sentAt), "MMM do yyyy") }}</td>
+        <td><button @click="email.archived = true">Archive</button></td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import { format } from "date-fns";
 export default {
   name: "App",
   data() {
     return {
+      format,
       emails: [
         {
           id: 1,
@@ -69,6 +72,16 @@ export default {
         }
       ]
     };
+  },
+  computed: {
+    sortedEmails() {
+      return this.emails.sort((e1, e2) => {
+        return e1.sentAt < e2.sentAt ? 1 : -1;
+      });
+    },
+    unarchivedEmails() {
+      return this.sortedEmails.filter(e => !e.archived);
+    }
   }
 };
 </script>
